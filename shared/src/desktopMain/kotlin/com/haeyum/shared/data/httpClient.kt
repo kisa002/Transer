e3ee0bed79f808f9bee3d96d3c67365e4313f42a
Holy-Knight <vnycall74@naver.com>
2023-11-24 03:40:05 +0900

    Refactor httpClient to expect/actual for iOS

---
+++ shared/src/desktopMain/kotlin/com/haeyum/shared/data/httpClient.kt
@@ -0,0 +1,33 @@
+package com.haeyum.shared.data
+
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.cio.CIO
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.logging.DEFAULT
+import io.ktor.client.plugins.logging.LogLevel
+import io.ktor.client.plugins.logging.Logger
+import io.ktor.client.plugins.logging.Logging
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
+
+actual val httpClient: HttpClient = HttpClient(CIO) {
+    install(ContentNegotiation) {
+        json(
+            Json {
+                prettyPrint = true
+                isLenient = true
+                coerceInputValues = true
+            }
+        )
+    }
+
+    install(Logging) {
+        logger = Logger.DEFAULT
+        level = LogLevel.ALL
+        logger = object : Logger {
+            override fun log(message: String) {
+                println(message)
+            }
+        }
+    }
+}
