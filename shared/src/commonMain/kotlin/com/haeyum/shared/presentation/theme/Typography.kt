c646a1ac171d24c92c80255dae04cebc8f988b5c
Holy-Knight <vnycall74@naver.com>
2023-12-05 19:46:46 +0900

    Refactor add and replace font

--- shared/src/commonMain/kotlin/com/haeyum/shared/presentation/theme/Typography.kt
+++ shared/src/commonMain/kotlin/com/haeyum/shared/presentation/theme/Typography.kt
@@ -2,15 +2,16 @@ package com.haeyum.shared.presentation.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.haeyum.transer.MR
+import dev.icerock.moko.resources.compose.asFont
 
 @Composable
-fun TranserTypography() = Typography(
-    body1 = TextStyle(
-        fontFamily = PretendardFontFamily(),
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp
-    )
-)
+fun TranserTypography() = Typography(defaultFontFamily = PretendardFontFamily())
+
+@Composable
+fun SuitTypography() = Typography(defaultFontFamily = SuitFontFamily())
